322eb784159488ab54fd4bcac8b57fdc9a67ee04
makhfuza <mahfuza_2000@mail.ru>
2020-03-12 15:05:27 +0000

    1 zadanie

--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -1,24 +1,15 @@
-#include <stdio.h>
 #include "swap.h"
 
-void Swap(char *left, char *right)
+void Swap( char *left, char *right)
 {
     char c;
     c=*left;
     *left=*right;
     *right=c;
-    printf("a=%d b=%d\n",*left,*right);
+
 }
 
-int main(void) 
-{
-  char x = 4, y = 9;
-
-  printf("x=%d y=%d\n",x,y);
-// передаём адреса переменных x и y в фунцию swap
-  Swap(&x,&y);
-
-  printf("x=%d y=%d\n",x,y);
-
-  return 0;
-}
+// компиляция программы в терминале gcc main.c swap.c -o swap 
+// main.c и swap.c программы, которые нужно скомпилировать 
+// swap название для скомпилированной программы
+// запуск программы ./swap
