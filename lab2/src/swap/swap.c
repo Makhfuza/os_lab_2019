7c1e73afa952d73d0ab2c06ede236a7de1d293bd
makhfuza <mahfuza_2000@mail.ru>
2020-03-12 14:29:36 +0000

    Правильное 1 Correct

--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -1,6 +1,24 @@
+#include <stdio.h>
 #include "swap.h"
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+    char c;
+    c=*left;
+    *left=*right;
+    *right=c;
+    printf("a=%d b=%d\n",*left,*right);
+}
+
+int main(void) 
+{
+  char x = 4, y = 9;
+
+  printf("x=%d y=%d\n",x,y);
+// передаём адреса переменных x и y в фунцию swap
+  Swap(&x,&y);
+
+  printf("x=%d y=%d\n",x,y);
+
+  return 0;
 }
